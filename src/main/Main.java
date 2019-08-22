bf39010e4ab91784da5c046fb93dce192398dfaf
Shrestha <shreshtha.tola_cs18@gla.ac.in>
2019-08-23 00:50:59 +0530

    input for time

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,11 +5,15 @@ package main;
  * @version 1.0
  */
 
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         double distance = 0;
         double time = 0;
         double speed = 0;
         double g = 9.8;
+        Scanner t = new Scanner(System.in);
+        time = t.nextDouble();
+
     }
 }
